4eb2f55614d9e691ccc6a99d5cdc047b11e364ba
赵永康 <2353392881@qq.com>
2022-08-11 20:38:35 +0800

    test01

---
+++ src/main/java/com/example/testgit/Testcontroller.java
@@ -0,0 +1,11 @@
+package com.example.testgit;
+
+/**
+ * @Author: zyk
+ * @CreateTime: 2022-08-11  20:37
+ * @Description: zzss
+ * @Version: 1.0
+ */
+public class Testcontroller {
+    public static final String word_test = "wordTest";
+}
